3c04bdf8f3e92bb348cfa6ea07f1009ff93d61a4
Louis <louis.perrin@diiage.org>
2024-03-06 13:35:12 +0100

    Add event model - title, description, date, location

--- Eval_D2_P2_back_louis_perrin_Core/Entities/Evenement.cs
+++ Eval_D2_P2_back_louis_perrin_Core/Entities/Evenement.cs
@@ -1,19 +1,10 @@
-﻿using Eval_D2_P2_back_louis_perrin_Core.Common;
-
-namespace Eval_D2_P2_back_louis_perrin_Core.Entities;
+﻿namespace Eval_D2_P2_back_louis_perrin_Core.Entities;
 
 public class Evenement
 {
-    public Guid id;
-    public string nomEvenement;
-    public ETypeEvenement typeEvenement;
+    public string title;
+    public string description;
+    public string location;
     public DateTime Date;
 
-    public Evenement(Guid id, string nomEvenement, ETypeEvenement typeEvenement, DateTime date)
-    {
-        this.id = id;
-        this.nomEvenement = nomEvenement;
-        this.typeEvenement = typeEvenement;
-        Date = date;
-    }
 }

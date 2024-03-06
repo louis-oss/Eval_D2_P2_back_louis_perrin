9df43e21babe38cf7447b58e82a97ff86fbc31fb
Louis <louis.perrin@diiage.org>
2024-03-06 13:30:34 +0100

    Initial commit - Azure Functions setup

---
+++ Eval_D2_P2_back_louis_perrin_Core/Common/ETypeEvenement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eval_D2_P2_back_louis_perrin_Core.Common;
+
+public enum  ETypeEvenement
+{
+    Concert,
+    Festival,
+    Spectacle,
+    Exposition,
+    Conference,
+    Atelier,
+    Autre
+}
---
+++ Eval_D2_P2_back_louis_perrin_Core/Entities/Evenement.cs
@@ -0,0 +1,19 @@
+﻿using Eval_D2_P2_back_louis_perrin_Core.Common;
+
+namespace Eval_D2_P2_back_louis_perrin_Core.Entities;
+
+public class Evenement
+{
+    public Guid id;
+    public string nomEvenement;
+    public ETypeEvenement typeEvenement;
+    public DateTime Date;
+
+    public Evenement(Guid id, string nomEvenement, ETypeEvenement typeEvenement, DateTime date)
+    {
+        this.id = id;
+        this.nomEvenement = nomEvenement;
+        this.typeEvenement = typeEvenement;
+        Date = date;
+    }
+}
